1f28c21f7266369de73d8d0fae686e0ce330dfe8
Thomas <51749973+Thovi98@users.noreply.github.com>
2024-03-09 10:46:42 +0100

    first try

---
+++ conf/start-ml.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -a
+. $IMMICH_PATH/env
+set +a
+
+cd $APP/machine-learning
+. venv/bin/activate
+
+: "\${MACHINE_LEARNING_HOST:=127.0.0.1}"
+: "\${MACHINE_LEARNING_PORT:=3003}"
+: "\${MACHINE_LEARNING_WORKERS:=1}"
+: "\${MACHINE_LEARNING_WORKER_TIMEOUT:=120}"
+
+exec gunicorn app.main:app \
+        -k app.config.CustomUvicornWorker \
+        -w "\$MACHINE_LEARNING_WORKERS" \
+        -b "\$MACHINE_LEARNING_HOST":"\$MACHINE_LEARNING_PORT" \
+        -t "\$MACHINE_LEARNING_WORKER_TIMEOUT" \
+        --log-config-json log_conf.json \
+        --graceful-timeout 0
